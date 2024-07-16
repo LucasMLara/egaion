efaced858ed79a3e32c67d8ce32dc414b85a78e6
Lucas M. Lara <LucasML@Live.com>
2024-07-16 12:39:54 -0300

    Login Page - Reponsive Design

---
+++ src/app/cadastrar/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function CADASTRO() {
+  return (
+    <>
+    <div>CADASTRO AQUI</div>
+    <Link href='/'>voltar</Link>
+    </>
+  )
+}
