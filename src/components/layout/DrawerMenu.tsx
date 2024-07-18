b423ee496924bbb9f836165f63af293b581a5e97
Lucas M. Lara <LucasML@Live.com>
2024-07-17 21:36:40 -0300

    add tooltips drawer

--- src/components/layout/DrawerMenu.tsx
+++ src/components/layout/DrawerMenu.tsx
@@ -4,18 +4,66 @@ import DocIcon from '@/assets/icons/Documentacao.svg'
 import EditalIcon from "@/assets/icons/Editais.svg"
 import EquipeIcon from "@/assets/icons/Equipe técnica.svg"
 import Image from 'next/image'
-
-
-
+import Link from "next/link"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 
 export default function DrawerMenu() {
   return (
-    <div className='w-24 h-screen bg-neutral-400 fixed flex flex-col items-center justify-evenly'>
-      <Image className='mx-3 size-6 hover:scale-110 transition-all' src={HomeIcon} alt=''></Image>
-      <Image className='mx-3 size-6 hover:scale-110 transition-all' src={EquipeIcon} alt='' ></Image>
-      <Image className='mx-3 size-6 hover:scale-110 transition-all' src={EditalIcon} alt=''></Image>
-      <Image className='mx-3 size-6 hover:scale-110 transition-all' src={DocIcon} alt=''></Image>
+    <div className='w-24 h-screen bg-neutral-400 fixed flex flex-col items-center justify-center gap-16'>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href='/home'>
+              <Image className='mx-3 size-6 hover:scale-110 transition-all' src={HomeIcon} alt=''></Image>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Início</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href='/team'>
+              <Image className='mx-3 size-6 hover:scale-110 transition-all' src={EquipeIcon} alt='' ></Image>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Equipe T.</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href='edital'>
+              <Image className='mx-3 size-6 hover:scale-110 transition-all' src={EditalIcon} alt=''></Image>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Editais</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href='docs'>
+              <Image className='mx-3 size-6 hover:scale-110 transition-all' src={DocIcon} alt=''></Image>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Doc.</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider> 
     </div>
   )
 }
