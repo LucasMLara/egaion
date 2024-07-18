b704ac54828d4fac698c9c0f5fbe11387309f866
Lucas M. Lara <LucasML@Live.com>
2024-07-17 21:54:48 -0300

    Edital card WIP

--- src/app/(AuthLayout)/home/page.tsx
+++ src/app/(AuthLayout)/home/page.tsx
@@ -3,14 +3,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <section className=' h-screen bg-primary-300 border-4 border-primary-800 relative grid place-content-center gap-6 ml-24'>
-      <div className="w-9 h-9 bg-primary-200" />
-      <div className="w-9 h-9 bg-primary-300" />
-      <div className="w-9 h-9 bg-primary-400" />
-      <div className="w-9 h-9 bg-primary-500" />
-      <div className="w-9 h-9 bg-primary-600" />
-      <div className="w-9 h-9 bg-primary-700" />
-      <div className="w-9 h-9 bg-primary-800" />
+    <section className=' h-screen bg-primary-300 border-4 border-primary-800  grid place-content-center gap-6 ml-24'>
+      
     </section>
   )
 }
---
+++ src/components/layout/EditalCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+interface IEditalCard {
+
+}
+export default function EditalCard() {
+  return (
+    <div>EditalCard</div>
+  )
+}
