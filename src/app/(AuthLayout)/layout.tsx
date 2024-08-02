30e9295a6adc70351451bd9ac410d63fd28dc6f7
nascjoao <oi@nasc.dev>
2024-08-02 17:10:30 -0300

    refactor: redirect user if session expired

--- src/app/(AuthLayout)/layout.tsx
+++ src/app/(AuthLayout)/layout.tsx
@@ -1,20 +1,12 @@
 import DrawerMenu from "@/components/layout/DrawerMenu";
 import Header from "@/components/layout/Header";
 import Content from "./content";
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth();
-
-  if (!session) {
-    redirect("/");
-  }
-
   return (
     <div className="grid grid-rows-[auto_1fr] h-svh overflow-y-hidden">
       <Header />
