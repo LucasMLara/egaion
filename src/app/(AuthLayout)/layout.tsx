149a7ec27ea548f2020e960b4c47f0b100c462f5
nascjoao <oi@nasc.dev>
2024-08-02 16:20:11 -0300

    refactor: Add authentication check to AuthLayout

--- src/app/(AuthLayout)/layout.tsx
+++ src/app/(AuthLayout)/layout.tsx
@@ -1,21 +1,27 @@
-import DrawerMenu from "@/components/layout/DrawerMenu"
-import Header from "@/components/layout/Header"
-import Content from "./content"
+import DrawerMenu from "@/components/layout/DrawerMenu";
+import Header from "@/components/layout/Header";
+import Content from "./content";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div className="grid grid-rows-[auto_1fr] h-svh overflow-y-hidden">
       <Header />
       <div className="grid grid-cols-[auto_1fr] h-full overflow-y-hidden">
         <DrawerMenu />
-        <Content>
-          {children}
-        </Content>
+        <Content>{children}</Content>
       </div>
     </div>
-  )
+  );
 }
