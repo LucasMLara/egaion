65cdd4a7ef0e6f22bc85557ef9677e02ff051a65
nascjoao <oi@nasc.dev>
2024-08-02 16:19:57 -0300

    feat: Add guest layout for handling unauthenticated users

---
+++ src/app/(GuestLayout)/layout.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default async function GuestLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  console.log(session);
+
+  if (session) {
+    redirect("/home");
+  }
+
+  return children;
+}
