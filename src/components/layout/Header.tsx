049f62b877187adbe96e773252e5eb0d071adf78
Lucas M. Lara <LucasML@Live.com>
2024-08-06 16:25:37 -0300

    ```text refactor: Add tooltip to user icon in Header component
    This commit adds a tooltip to the user icon in the Header component. The tooltip provides additional information about the content that will be accessed when clicking on the icon. This improves the user experience by providing clarity and guidance.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,6 +5,13 @@ import Shadow from "./Shadow";
 import { auth, signOut } from "@/auth";
 // import Image from "next/image";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export default async function Header() {
   // const session = await auth();
   return (
@@ -17,16 +24,33 @@ export default async function Header() {
         </Link>
       </div>
       <div className="h-full w-full flex justify-end items-center">
-        <Link href="#">
-          {/* <Image
+        {/* <Link href="#">
+          <Image
             src={session?.user.image}
             alt={session?.user.name}
             width={20}
             height={20}
-          /> */}
+          />
           <UserIcon className="mx-3 size-6 hover:scale-110 transition-all hover:stroke-primary-400" />
-        </Link>
+        </Link> */}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="">
+                <UserIcon className="mx-3 size-6 hover:scale-110 transition-all hover:stroke-primary-400" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="text-xl">
+                Qual conteúdo iremos acessar ao clicar nesse ícone ? as
+                Informações da empresa? Quais dados da empresa podem ser
+                exibidos, quais podem ser alterados?
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <form
+          className="flex"
           action={async () => {
             "use server";
             await signOut({
