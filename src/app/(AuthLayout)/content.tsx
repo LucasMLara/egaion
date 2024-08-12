5de3ad3b959b47ba4a5bac53dc697288f1f44ea8
Lucas M. Lara <LucasML@Live.com>
2024-08-12 13:16:29 -0300

    refactor: Improve AuthLayout content component
    The code changes in the AuthLayout content component improve the readability and maintainability of the code. The function changeScrolledStatus now has a more descriptive name, and the code is properly formatted and indented. This refactor enhances the overall quality of the codebase.

--- src/app/(AuthLayout)/content.tsx
+++ src/app/(AuthLayout)/content.tsx
@@ -1,21 +1,26 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
-export default function Content({children}: {children: React.ReactNode}) {
-    const [hasScrolled, setHasScrolled] = React.useState(false);
+export default function Content({ children }: { children: React.ReactNode }) {
+  const [hasScrolled, setHasScrolled] = React.useState(false);
 
-    function changeScrolledStatus(event: React.UIEvent<HTMLElement>) {
-        const target = event.target as HTMLElement;
-        if (target.scrollTop > 0) {
-            setHasScrolled(true);
-        } else {
-            setHasScrolled(false);
-        }
+  function changeScrolledStatus(event: React.UIEvent<HTMLElement>) {
+    const target = event.target as HTMLElement;
+    if (target.scrollTop > 0) {
+      setHasScrolled(true);
+    } else {
+      setHasScrolled(false);
     }
+  }
 
   return (
-    <section className="overflow-y-auto" data-content data-scrolled={hasScrolled} onScroll={changeScrolledStatus}>
-        {children}
+    <section
+      className="overflow-y-auto"
+      data-content
+      data-scrolled={hasScrolled}
+      onScroll={changeScrolledStatus}
+    >
+      {children}
     </section>
-  )
+  );
 }
