d703552c81c00494b38d550f85fbe5daa10f7164
Lucas M. Lara <LucasML@Live.com>
2024-08-16 15:31:23 -0300

    Remove logo do sebrae no header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,9 +19,9 @@ export default async function Header() {
       className={`relative w-full h-16 flex justify-between bg-neutral-400 z-50`}
     >
       <div className="h-full w-full flex items-center">
-        <Link href="https://sebrae.com.br/sites/PortalSebrae/" target="_blank">
+        {/* <Link href="https://sebrae.com.br/sites/PortalSebrae/" target="_blank">
           <SebraeLogo className="size-14 mx-6 hover:scale-110 transition-all" />
-        </Link>
+        </Link> */}
       </div>
       <div className="h-full w-full flex justify-end items-center">
         {/* <Link href="#">
