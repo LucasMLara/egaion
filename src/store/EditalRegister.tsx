6a533051ce147c282ea25b2c95a042be8e6287e3
Lucas M. Lara <LucasML@Live.com>
2024-08-22 10:43:41 -0300

    feat: 🚧 Installing and applying zustand

---
+++ src/store/EditalRegister.tsx
@@ -0,0 +1,61 @@
+import { create } from "zustand";
+import { IConsultant } from "@/types/types";
+import { persist } from "zustand/middleware";
+
+type Documents = {
+  title: string;
+  blob: string;
+  id: string;
+};
+
+type History = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+type IEditalStore = {
+  editalData: {
+    Consultores: IConsultant[];
+    Documentos: Documents[];
+    Historico: History[];
+    Anexos: Documents[];
+  };
+};
+
+type editalActions = {
+  cadastrarConsultor: (consultor: IConsultant) => void;
+  reset: () => void;
+};
+
+const initialState = {
+  editalData: {
+    Consultores: [],
+    Documentos: [],
+    Historico: [],
+    Anexos: [],
+  },
+};
+
+export const useEditalStore = create<IEditalStore & editalActions>()(
+  persist(
+    (set, get) => ({
+      editalData: {
+        Consultores: get()?.editalData?.Consultores ?? [],
+        Documentos: get()?.editalData?.Documentos ?? [],
+        Historico: get()?.editalData?.Historico ?? [],
+        Anexos: get()?.editalData?.Anexos ?? [],
+      },
+      reset: () => {
+        set(initialState);
+      },
+      permitirEnviar: false,
+      cadastrarConsultor: (consultor) =>
+        set((state) => ({
+          editalData: { ...state.editalData },
+          Consultores: [...state.editalData.Consultores, consultor],
+        })),
+    }),
+    { name: "editalStorage" }
+  )
+);
