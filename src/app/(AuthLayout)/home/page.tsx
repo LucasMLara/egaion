06f9ec9b200424373e69356f138baab1a55041a2
Lucas M. Lara <LucasML@Live.com>
2024-09-12 01:16:31 -0300

    Refactor: Simplify rendering logic in Home page component to show only registered editals

--- src/app/(AuthLayout)/home/page.tsx
+++ src/app/(AuthLayout)/home/page.tsx
@@ -10,20 +10,18 @@ export default function Home() {
   const { myEditals, setListMyEditals } = useMyEditals();
 
   useEffect(() => {
-    setListMyEditals(
-      generateEditalCardData().map(
-        (edital): IMyEditals => ({
-          id: edital.editalId,
-          date: new Date(edital.editalCardDate),
-          title: edital.editalCardTitle,
-          description: edital.editalCardContent,
-          status: edital.status,
-        })
-      )
-    );
+    setListMyEditals(myEditals);
   }, []);
 
-  return (
+  return myEditals.length === 0 ? (
+    <div className="flex flex-col h-full items-center justify-center">
+      <div className="text-center flex w-5/6 md:w-3/4 flex-col rounded-md border-2 bg-neutral-500 justify-center items-center p-14">
+        <h1 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          Você não se cadastrou em nenhum edital
+        </h1>
+      </div>
+    </div>
+  ) : (
     <section className="flex flex-wrap gap-6 py-10 items-center justify-center flex-col">
       <h1 className="text-2xl font-bold text-neutral-700 text-center m-9">
         Meus Editais
