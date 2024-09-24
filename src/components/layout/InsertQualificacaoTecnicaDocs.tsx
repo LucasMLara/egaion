f2555df4561926d8c0f4161b05102a7dd0766463
Lucas M. Lara <LucasML@Live.com>
2024-09-24 11:34:42 -0300

    Refactor: Separate logic for selecting areas and rendering area cards
    This commit refactors the InsertQualificacaoTecnicaDocs component to divide it into separate components for selecting areas and rendering area cards. This separation allows for better organization and maintenance of the internal logic of each component.
    
    Changes:
    - Removed unnecessary destructuring of the areas property from the editalData object
    - Updated the mapping of Qualificacao to use the areaId and name properties
    - Updated the key prop of the AreaCard component to use the areaId property

--- src/components/layout/InsertQualificacaoTecnicaDocs.tsx
+++ src/components/layout/InsertQualificacaoTecnicaDocs.tsx
@@ -4,19 +4,15 @@ import AreaCard from "./AreaCard";
 import { useEditalStore } from "@/store/EditalRegister";
 
 export default function InsertQualificacaoTecnicaDocs() {
-  const {
-    editalData: {
-      Qualificacao: { areas },
-    },
-  } = useEditalStore();
+  const { Qualificacao } = useEditalStore();
 
   return (
     <div>
       <AccreditationForm data={accreditationData} />
-      {areas.length > 0 && (
+      {Qualificacao.length > 0 && (
         <div className="flex flex-wrap gap-3 place-content-center">
-          {areas.map((area) => (
-            <AreaCard key={area} area={area} />
+          {Qualificacao.map((area) => (
+            <AreaCard key={area.areaId} area={area.name} areaId={area.areaId} />
           ))}
         </div>
       )}
