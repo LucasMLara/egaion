87c5403483e171f82ffe6ad137284fa112e56828
Lucas M. Lara <LucasML@Live.com>
2024-10-11 12:30:48 -0300

    Refactor: Update middleware config to include new routes for team and member registration

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,6 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|cadastrar|esquecisenha|recadastrarsenha).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|cadastrar|esquecisenha|recadastrarsenha|team|memberregister).*)",
   ],
 };
