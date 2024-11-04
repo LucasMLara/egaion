7e48c3dfa320afa51c15ef74440a4eadb93a5b5d
Lucas M. Lara <LucasML@Live.com>
2024-11-04 18:09:58 -0300

    Add: Implement back navigation button in TeamMember component

--- src/app/(AuthLayout)/team/[userId]/page.tsx
+++ src/app/(AuthLayout)/team/[userId]/page.tsx
@@ -6,12 +6,14 @@ import { Input } from "@/components/ui/input";
 import { getData } from "@/components/ConsultantTable/ConsultantTable";
 import { maskCpf } from "@/lib/utils";
 import { ConsultantRowDisplay } from "@/types/types";
+import { useRouter } from "next/navigation";
 
 import { LoaderIcon } from "lucide-react";
 
 export default function TeamMember({ params }: { params: { userId: string } }) {
   const { userId } = params;
   const [member, setMember] = useState<ConsultantRowDisplay[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const data = getData(userId);
@@ -24,7 +26,6 @@ export default function TeamMember({ params }: { params: { userId: string } }) {
         <LoaderIcon className="animate-spin size-14 text-primary-400" />
       </div>
     );
-
   return (
     <section className="my-6">
       <h1 className="text-2xl font-bold text-neutral-700 text-center">
@@ -76,7 +77,10 @@ export default function TeamMember({ params }: { params: { userId: string } }) {
             />
           </div>
         </div>
-        <div className="w-full flex justify-end mt-4">
+        <div className="w-full flex justify-end mt-4 gap-4">
+          <Button onClick={() => router.back()} variant="outline" type="button">
+            Voltar
+          </Button>
           <Button
             type="submit"
             className="bg-gradient-primary hover:shadow-lg hover:shadow-gray-500/40 transition-all disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-auto disabled:shadow-none w-64"
