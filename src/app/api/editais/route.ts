7b6f5a8e9eaa6cbd44d482e3ee8fe55c72ebd9ca
Lucas M. Lara <LucasML@Live.com>
2024-12-09 15:48:28 -0300

    feat: Adicionar endpoint GET para buscar editais com sanitização de dados

---
+++ src/app/api/editais/route.ts
@@ -0,0 +1,25 @@
+
+import prisma from "@/db";
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const editais = await prisma.wFUSER.findMany();
+        const sanitizedEditais = editais.map((edital) =>
+            Object.fromEntries(
+                Object.entries(edital).map(([key, value]) => [
+                    key,
+                    typeof value === "bigint" ? value.toString() : value,
+                ])
+            )
+        );
+        return NextResponse.json({ editais: sanitizedEditais });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return NextResponse.json(
+            { error: "Failed to fetch data" },
+            { status: 500 }
+        );
+    }
+}
