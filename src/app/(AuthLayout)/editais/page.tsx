e2f3c009bb9cbf4d429a3ebc102bcca8ce8ef507
Lucas M. Lara <LucasML@Live.com>
2024-12-27 12:06:19 -0300

    feat: Atualizar lógica de busca e exibição de editais com novos campos e formatação de data

--- src/app/(AuthLayout)/editais/page.tsx
+++ src/app/(AuthLayout)/editais/page.tsx
@@ -1,40 +1,31 @@
 "use client";
-import React, { useEffect } from "react";
+import { useEffect} from "react";
 import EditalCard from "@/components/layout/EditalCard";
-import { generateEditalCardData } from "@/lib/utils";
-import { IAvailableEditals } from "@/store/useAvailableEditals/types";
 import { useAvailableEditals } from "@/store/useAvailableEditals";
+import  Dayjs from "dayjs"
 
 export default function Editais() {
   const { availableEditals, setListEditals } = useAvailableEditals();
 
-  // useEffect(() => {
-  //   async function fetchEditais() {
-  //     try {
-  //       const response = await fetch("/api/editais");
-  //       const data = await response.json();
-  //       console.log(data)
-        
-  //     } catch (error) {
-  //       console.log('Erro', error);
-        
-  //     }
-  //   }
-  //   fetchEditais();
-  // }, [setListEditals]);
-
   useEffect(() => {
-    setListEditals(
-      generateEditalCardData().map(
-        (edital): IAvailableEditals => ({
-          id: edital.editalId,
-          date: new Date(edital.editalCardDate),
-          title: edital.editalCardTitle,
-          description: edital.editalCardContent,
-        })
-      )
-    );
-  }, []);
+    async function fetchEditais() {
+      try {
+        const response = await fetch("/api/editais");
+        const { editais } = await response.json();
+        setListEditals(editais);
+        
+      } catch (error) {
+        console.log('Erro', error);
+        
+      }
+    }
+    fetchEditais();
+  }, [setListEditals]);
+
+
+  
+
+
   return (
     <section className="flex flex-wrap gap-6 py-10 items-center justify-center flex-col">
       <h1 className="text-2xl font-bold text-neutral-700 text-center m-9">
@@ -44,14 +35,14 @@ export default function Editais() {
         {availableEditals.map((card) => (
           <EditalCard
             status={null}
-            key={card.id}
-            editalCardContent={card.description}
-            editalCardTitle={card.title}
-            editalCardDate={card.date.toLocaleDateString("pt-BR")}
-            editalDialogTitle={card.title}
-            editalDialogDescription={card.description}
-            editalDialogContent={card.description}
-            editalId={card.id}
+            key={card.idSCEdital}
+            editalCardContent={card.ResumoEdital}
+            editalCardTitle={card.NomeEdital}
+            editalCardDate={Dayjs(card.InicioEdital).format("DD/MM/YYYY")}
+            editalDialogTitle={card.NomeEdital}
+            editalDialogDescription={card.Objetivos}
+            editalDialogContent={card.Objetivos}
+            editalId={card.idSCEdital}
           />
         ))}
       </div>
