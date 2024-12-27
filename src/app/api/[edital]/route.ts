2e8c61b53c8b1d499545209dd350a242fa5e83bc
Lucas M. Lara <LucasML@Live.com>
2024-12-27 14:44:33 -0300

    feat: Implementar endpoint GET para buscar editais por ID com tratamento de erros

---
+++ src/app/api/[edital]/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/db";
+import { NextResponse } from "next/server";
+
+export async function GET(_: Request, { params }: { params: { edital: string } }) {
+    const { edital } = params;
+
+    try {
+        if (!edital || isNaN(Number(edital))) {
+            return NextResponse.json({ error: "ID do edital inválido ou Inexistente" }, { status: 400 });
+        }
+
+        const foundEdital = await prisma.sCEdital.findFirst({
+            where: {
+                idSCEdital: parseInt(edital),
+            },
+        });
+
+        if (!foundEdital) {
+            return NextResponse.json({ message: "Nenhum edital com este ID foi encontrado" }, { status: 404 });
+        }
+
+        const serializedEdital = JSON.parse(
+            JSON.stringify(foundEdital, (_, value) =>
+                typeof value === "bigint" ? value.toString() : value
+            )
+        );
+
+        return NextResponse.json(serializedEdital);
+    } catch (error: any) {
+        console.error("Error fetching data:", error);
+        return NextResponse.json(
+            { error: `Failed to fetch data: ${error.message}` },
+            { status: 500 }
+        );
+    }
+}
