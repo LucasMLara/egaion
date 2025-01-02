7531ab7a519473152faaff450f26580eac5ec63a
Lucas M. Lara <LucasML@Live.com>
2025-01-02 13:01:24 -0300

    feat: Integrar histórico de edital e exibir utilizando componente EditalHistory

--- src/app/(AuthLayout)/[editalId]/page.tsx
+++ src/app/(AuthLayout)/[editalId]/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { currentDate, formatDate } from "@/lib/utils";
+import { formatDate } from "@/lib/utils";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { history } from "@/mocks";
 import Attachments from "@/components/layout/Attachments";
 import InsertEditalDocuments from "@/components/layout/InsertEditalDocuments";
 import { useEditalStore } from "@/store/EditalRegister";
@@ -12,6 +11,8 @@ import InsertQualificacaoTecnicaDocs from "@/components/layout/InsertQualificaca
 import ConsultantsArea from "@/components/layout/ConsultantsArea";
 import { FileText, File, UserCheck, Paperclip, History } from "lucide-react";
 import { IAvailableEdital } from "@/store/useAvailableEditals/types";
+import EditalHistory from "@/components/layout/EditalHistory";
+import { HistoryItem } from "@/types/types";
 
 
 
@@ -26,8 +27,10 @@ export default function EditalId({
   const { permissaoDeCadastroEdital, Consultores, Documentos, Qualificacao } =
     useEditalStore();
 
-  const initialCurrentEditalState = {
-    idSCEdital: "",
+  const initialCurrentEditalState : IAvailableEdital = {
+    serializedEdital: {
+
+      idSCEdital: "",
       finalEnt: 0,
       baCreatedTime: "",
       baGuid: "",
@@ -53,20 +56,33 @@ export default function EditalId({
       ResumoEdital: "",
       ConfirmarDadosCadastro: false,
       SelecionadoExclusao: "",
+    },
       serializedEditalHistory: []
   }
     const [currentEdital, setCurrentEdital] = useState<IAvailableEdital>(initialCurrentEditalState);
+    const [editalHistory, setEditalHistory] = useState<HistoryItem[]>([]);
+    
+    const history = useMemo(() => {
+      return editalHistory.map((itemHistorico) => {
+        return {
+          DataCriacao: formatDate(itemHistorico.DataCriacao),
+          Descricao: itemHistorico.Descricao,
+          Responsavel: itemHistorico.Responsavel,
+          idHistorico: itemHistorico.idHistorico,
+        };
+      })
+    }, [editalHistory]);
 
     useEffect(() => {
       async function fetchEdital() {
           try {
               const response = await fetch(`/api/${params.editalId}`);
               const data = await response.json();
-              setCurrentEdital(data);
               if (!response.ok) {
                   console.error("Error:", data.error || data.message);
               } else {
-                  console.log("Edital data:", data);
+                  setCurrentEdital(data);
+                  setEditalHistory(data.serializedEditalHistory);
               }
           } catch (error) {
               console.error("Fetch error:", error);
@@ -77,8 +93,9 @@ export default function EditalId({
       }
   }, [params.editalId]);
   
-  console.log("Current Edital:", currentEdital.serializedEditalHistory);
   
+  
+
   return (
     <section className="h-full">
       <Tabs defaultValue="content" className="w-auto m-4">
@@ -118,18 +135,7 @@ export default function EditalId({
           <InsertQualificacaoTecnicaDocs />
         </TabsContent>
         <TabsContent value="historico">
-          <h2 className="text-lg text-center font-bold mb-4">Histórico</h2>
-          <div className="border-2 rounded-xl overflow-auto m-4 bg-neutral-300 text-neutral-600 h-full">
-            <ul className="m-4">
-              {history.map((item, index) => (
-                <li key={index} className="m-4">
-                  <span> {item.date}</span>
-                  <h3 className="text-lg font-bold">{item.title}</h3>
-                  <p>{item.status}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
+      <EditalHistory historico={history}/>
         </TabsContent>
         <TabsContent value="anexos">
           <h2 className="text-lg text-center font-bold mb-4">Anexos</h2>
@@ -140,9 +146,9 @@ export default function EditalId({
           <div className="border-2 rounded-xl overflow-auto m-4 bg-neutral-300 text-neutral-600 h-full">
             <h1 className="text-center m-4 text-2xl font-bold">
               {" "}
-              Edital : {currentEdital.NomeEdital}{" "}
+              Edital : {currentEdital.serializedEdital.NomeEdital}{" "}
             </h1>
-            <h2 className="text-center text-lg">Data: {formatDate(currentEdital.InicioEdital)}</h2>
+            <h2 className="text-center text-lg">Data: {formatDate(currentEdital.serializedEdital.InicioEdital)}</h2>
             <h1 className="text-center text-md font-semibold my-3">
               Clique no Link abaixo para visualizar as informaçoes atualizadas
               do edital.
