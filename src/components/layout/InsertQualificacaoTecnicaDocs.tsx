1120dc9e5d2689141c8a3f0cda6dc9a2e55463ae
Lucas M. Lara <LucasML@Live.com>
2025-01-10 10:39:54 -0300

    feat: Atualizar componente InsertQualificacaoTecnicaDocs para aceitar áreas como propriedade

--- src/components/layout/InsertQualificacaoTecnicaDocs.tsx
+++ src/components/layout/InsertQualificacaoTecnicaDocs.tsx
@@ -3,8 +3,13 @@ import { accreditationData } from "@/mocks";
 import AreaCard from "./AreaCard";
 import { useEditalStore } from "@/store/EditalRegister";
 import { useEffect } from "react";
+import { OutputItem } from "@/lib/utils";
 
-export default function InsertQualificacaoTecnicaDocs() {
+export default function InsertQualificacaoTecnicaDocs({
+  areas,
+}: {
+  areas: OutputItem[];
+}) {
   const { Qualificacao, alterarPermissaoEdital, Documentos, Consultores } =
     useEditalStore();
 
@@ -25,11 +30,11 @@ export default function InsertQualificacaoTecnicaDocs() {
 
   return (
     <div className="max-h-screen overflow-auto">
-      <AccreditationForm data={accreditationData} />
+      <AccreditationForm data={areas} />
       {Qualificacao.length > 0 && (
         <div className="flex flex-wrap gap-3">
           {Qualificacao.map((area) => (
-            <AreaCard key={area.areaId} area={area.name} areaId={area.areaId}  />
+            <AreaCard key={area.areaId} area={area.name} areaId={area.areaId} />
           ))}
         </div>
       )}
