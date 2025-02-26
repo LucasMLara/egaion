68d7a09873f7ce2fef0ade606d9f9c450fe39488
Lucas Muniz Lara <lucasml@live.com>
2025-02-26 20:47:46 -0300

    feat: Implement SOAP request for Edital registration and add loading state to confirmation button

--- src/app/(AuthLayout)/[editalId]/confirmarDados/page.tsx
+++ src/app/(AuthLayout)/[editalId]/confirmarDados/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,7 +10,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import EditalRegister from "@/services/editalRegister";
 import { useRouter } from "next/navigation";
 import { useEditalStore } from "@/store/EditalRegister";
 import { useSession } from "next-auth/react";
@@ -18,8 +18,12 @@ import {
   prepararConsultoresCredenciada,
   prepararAreasCredenciada,
 } from "@/lib/concatEditalDocuments";
+import { toast } from "sonner";
+import { LoaderIcon } from "lucide-react";
+
 
 export default function ConfirmaDados() {
+  const [loading, setLoading] = useState(false)
   
   const router = useRouter();
   const { Consultores, Qualificacao, Documentos, currentEditalId } = useEditalStore();
@@ -29,11 +33,64 @@ export default function ConfirmaDados() {
   const idScCredenciada = data?.user.idSCCredenciada
   
   async function enviarDadosEdital() {
-    const documentosTratados = await prepararDocumentosCredenciada(Documentos)
-    const consultoresTratados = await prepararConsultoresCredenciada(Consultores, idScCredenciada)
-    const  qualificacaoTratada = await prepararAreasCredenciada(Qualificacao)
-    
-    EditalRegister({Documentos: documentosTratados, Qualificacao: qualificacaoTratada, Consultores: consultoresTratados, currentEditalId})
+    const url = "http://192.168.2.149/EGAION/webservices/workflowenginesoa.asmx";
+    const body = `
+        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:tem="http://tempuri.org/">
+          <soap:Header/>
+            <soap:Body>
+                <tem:createCases>
+                  <tem:casesInfo>
+                    <BizAgiWSParam>
+                        <Cases>
+                          <Case>
+                        <Process>CadastrarCredenciadaNoEdit</Process>
+                                  <Entities>
+                                      <SCCredenciadasEdital>
+                                        <StatusCadastro entityName="SCStatusCredEdital" businessKey="Codigo='3'"/>
+                                            <SCEdital businessKey="idSCEdital='${currentEditalId}'"/>
+                                            <Credenciada entityName="SCCredenciada" businessKey="idSCCredenciada='${idScCredenciada}'"/>
+                                              ${await prepararDocumentosCredenciada(Documentos)}
+                                            ${await prepararAreasCredenciada(Qualificacao)}
+                                                ${await prepararConsultoresCredenciada(Consultores, idScCredenciada)}
+                                      </SCCredenciadasEdital>
+                            </Entities>
+                            </Case>
+                          </Cases>
+                        </BizAgiWSParam>
+                      </tem:casesInfo>
+                    </tem:createCases>
+            </soap:Body>
+  </soap:Envelope>
+    `;
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type":
+          'application/soap+xml;charset=UTF-8;action="http://tempuri.org/createCases"',
+        "Accept-Encoding": "gzip,deflate",
+      },
+      body,
+    };
+
+    try{
+      setLoading(true)
+      const response = await fetch(url, fetchOptions);
+      if (!response.ok) {
+        throw new Error("Erro ao se Registrar no Edital.");
+      }
+      toast.success('Registro no Edital Bem sucedido!')
+      router.push('/')
+      setLoading(false)
+    }catch(e) {
+      if (e instanceof Error) {
+        toast.error(e.message);
+      } else {
+        toast.error("An unknown error occurred.");
+      }
+      console.error(e)
+      setLoading(false)
+    }
 }
   return (
     <div className="rounded w-full md:w-4/6 bg-neutral-500 flex flex-col items-center content-center md:mx-auto mt-14 p-5">
@@ -61,14 +118,14 @@ export default function ConfirmaDados() {
               </DialogDescription>
             </DialogHeader>
             <DialogFooter>
-              <form action={() => enviarDadosEdital()}>
               <Button
-                type="submit"
+                onClick={ () => enviarDadosEdital()}
+                disabled={loading}
                 className="bg-gradient-primary"
+
               >
-                Confirmar
+                {loading ? <LoaderIcon className="animate-spin" /> : "Confirmar"}
               </Button>
-              </form>
             </DialogFooter>
           </DialogContent>
         </Dialog>
