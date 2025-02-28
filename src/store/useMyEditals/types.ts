fe9e63e3a0b70bb5841bfb4e05dce67fccc96cfd
Lucas Muniz Lara <lucasml@live.com>
2025-02-28 15:35:34 -0300

    feat: Extend IMyEditals interface with additional fields for enhanced Edital data management

--- src/store/useMyEditals/types.ts
+++ src/store/useMyEditals/types.ts
@@ -1,11 +1,56 @@
 type TEditalStatus = "pending" | "ok" | "error" | null;
 
 export interface IMyEditals {
-  id: string;
-  date: Date;
-  title: string;
-  description: string;
-  status: TEditalStatus;
+  idSCEdital: string;
+  ObjetoEditalBase64: string;
+  finalEnt: number;
+  baCreatedTime: string;
+  baGuid: string;
+  QuantidadeMinimaLocalidade: number | null;
+  QuantidadeMaximaLocalidade: number | null;
+  LocalidadePadrao: string | null;
+  DocumentacaoPadrao: string;
+  ObjetoEdital: string | null;
+  TipodeCredenciamento: string;
+  NomeEdital: string;
+  FimEdital: string;
+  Credenciadora: string;
+  TipoChamadas: string;
+  InicioEdital: string;
+  Objetivos: string;
+  EscolhaLocalidades: boolean;
+  Status: string | null;
+  PesquisaNome: string | null;
+  PesquisaCNPJ: string | null;
+  PesquisaStatusCred: string | null;
+  PesquisaCredenciada: string | null;
+  ObjetoEditalArquivo: number;
+  ResumoEdital: string;
+  ConfirmarDadosCadastro: boolean;
+  SelecionadoExclusao: string | null;
+  serializedEditalHistory: {
+    idHistorico: string;
+    finalEnt: number;
+    baCreatedTime: string;
+    baGuid: string;
+    DadosPadrao: string | null;
+    Responsavel: string;
+    DataCriacao: string;
+    Descricao: string;
+    SCEdital: string;
+  }[];
+  serializedEditalAttachments: {
+    Arquivo: number;
+    DadosPadrao: null;
+    DataCriacao: string;
+    Descricao: string;
+    Responsavel: string;
+    SCEdital: string;
+    baCreatedTime: string;
+    baGuid: string;
+    finalEnt: number;
+    idAnexo: string;
+  }[];
 }
 
 export type TState = {
