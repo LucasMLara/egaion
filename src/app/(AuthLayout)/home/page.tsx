3920aadff0daa0847836343e9301238369b82764
Lucas Muniz Lara <lucasml@live.com>
2025-02-28 15:36:50 -0300

    feat: Implement loading state and fetch Editais data with improved error handling

--- src/app/(AuthLayout)/home/page.tsx
+++ src/app/(AuthLayout)/home/page.tsx
@@ -1,15 +1,41 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import MyEditalCard from "@/components/layout/MyEditalCard";
 import { useMyEditals } from "@/store/useMyEditals";
+import { LoaderIcon } from "lucide-react";
+import { formatDate } from "@/lib/utils";
 
 export default function Home() {
   const { myEditals, setListMyEditals } = useMyEditals();
+    const [loading, setLoading] = useState(true);
+  
+
+const fetchMyEditals = useCallback(async () => {
+  try{
+    const response = await fetch('api/myEditals');
+    const { meusEditais } = await response.json();
+    setListMyEditals(meusEditais)
+    setLoading(false)
+  }catch(e) {
+    console.log('Erro desconhecido', e)
+  }
+} , [setListMyEditals ])
+
+  useEffect(() => {
+  fetchMyEditals()
+
+}, [fetchMyEditals])
 
   useEffect(() => {
     setListMyEditals(myEditals);
   }, [setListMyEditals, myEditals]);
-
+  if (loading) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <LoaderIcon className="animate-spin size-8" />
+      </div>
+    );
+  }
   return myEditals.length === 0 ? (
     <div className="flex flex-col h-full items-center justify-center">
       <div className="text-center flex w-5/6 md:w-3/4 flex-col rounded-md border-2 bg-neutral-500 justify-center items-center p-14">
@@ -26,15 +52,15 @@ export default function Home() {
       <div className="flex flex-wrap gap-6 py-10 place-content-center">
         {myEditals.map((card) => (
           <MyEditalCard
-            key={card.id}
-            status={card.status}
-            editalCardContent={card.description}
-            editalCardTitle={card.title}
-            editalCardDate={card.date.toLocaleDateString("pt-BR")}
-            editalDialogTitle={card.title}
-            editalDialogDescription={card.description}
-            editalDialogContent={card.description}
-            editalId={card.id}
+            status={null}
+                key={card.idSCEdital}
+                editalCardContent={card.ResumoEdital}
+                editalCardTitle={card.NomeEdital}
+                editalCardDate={formatDate(card.InicioEdital)}
+                editalDialogTitle={card.NomeEdital}
+                editalDialogDescription={card.Objetivos}
+                editalDialogContent={card.Objetivos}
+                editalId={card.idSCEdital}
           />
         ))}
       </div>
