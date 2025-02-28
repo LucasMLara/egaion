d930b51e26823a012c602b5ac7468e5144c9a983
Lucas Muniz Lara <lucasml@live.com>
2025-02-28 13:16:40 -0300

    feat: Optimize Editais fetching with raw SQL query and improve error handling

--- src/app/api/myEditals/route.ts
+++ src/app/api/myEditals/route.ts
@@ -1,43 +1,50 @@
-
 import { auth } from "@/auth";
 import prisma from "@/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        const session = await auth();
+  try {
+    const session = await auth();
 
-        if (!session || !session.user?.idSCCredenciada) {
-            return NextResponse.json(
-                { error: "Usuario não autenticado" },
-                { status: 401 }
-            );
-        }
-
-        const userId = session.user.idSCCredenciada;
-
-        const editais = await prisma.sCCredenciadasEdital.findMany({
-            where: {
-                Credenciada: BigInt(userId)
-            },
-        });
-
-
-        const sanitizedEditais = editais.map((edital) =>
-            Object.fromEntries(
-                Object.entries(edital).map(([key, value]) => [
-                    key,
-                    typeof value === "bigint" ? value.toString() : value,
-                ])
-            )
-        );
-
-        return NextResponse.json({ meusEditais: sanitizedEditais });
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return NextResponse.json(
-            { error: "Failed to fetch data" },
-            { status: 500 }
-        );
+    if (!session || !session.user?.idSCCredenciada) {
+      return NextResponse.json(
+        { error: "Usuario não autenticado" },
+        { status: 401 }
+      );
     }
+
+    const userId = session.user.idSCCredenciada;
+
+    const editais: Array<{ [key: string]: any }> = await prisma.$queryRaw`
+    SELECT 
+      sce.idSCCredenciadasEdital,
+      sce.SCEdital,
+      sce.StatusCadastro,
+      sce.JustificativaNaoAprovacao,
+      se.NomeEdital,
+      se.InicioEdital,
+      se.FimEdital,
+      se.Status
+    FROM sCCredenciadasEdital sce
+    JOIN sCEdital se ON sce.SCEdital = se.idSCEdital
+    WHERE sce.Credenciada = ${BigInt(userId)}
+  `;
+
+    const sanitizedEditais = editais.map((edital) =>
+      Object.fromEntries(
+        Object.entries(edital).map(([key, value]) => [
+          key,
+          typeof value === "bigint" ? value.toString() : value,
+        ])
+      )
+    );
+
+    return NextResponse.json({ meusEditais: sanitizedEditais });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
 }
