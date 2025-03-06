d6288c74e79a23fc8b12dc113236ded761f22c26
Lucas Muniz Lara <lucasml@live.com>
2025-03-06 17:06:25 -0300

    fix: Update footer to use new logo and adjust layout for better alignment

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import PentagoLogo from "@/assets/Pentago.svg";
+import DesenvolvidoPorPentago from "@/assets/DesenvolvidoPorPentago.svg"
 
 import Link from "next/link";
 export default function Footer() {
   return (
     <footer className={`relative w-full h-16 flex justify-between bg-neutral-400 z-50`}>
-      <div className='flex justify-center items-center w-full'>
-      <p className="text-sm text-muted-foreground">Desenvolvido por:</p>
+      <div className='flex justify-end items-center w-full'>
       <Link href="https://pentago.com.br/novo/" target="_blank">
-          <PentagoLogo className="size-16 mx-6 hover:scale-110 transition-all" />
-        </Link>
+          <DesenvolvidoPorPentago className="mx-6 size-20 hover:scale-110 transition-all"/>
+      </Link>
       </div>
     </footer>
   )
