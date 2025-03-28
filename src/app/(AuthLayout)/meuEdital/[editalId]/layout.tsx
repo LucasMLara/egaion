c44f865f49776776e20d12fad5200556385246dc
Lucas M. Lara <LucasML@Live.com>
2025-03-28 14:49:50 -0300

    feat: Add Layout component with Suspense for loading state in edital pages

---
+++ src/app/(AuthLayout)/meuEdital/[editalId]/layout.tsx
@@ -0,0 +1,13 @@
+import React, { Suspense } from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function Layout({children}: {children: React.ReactNode}) {
+  return (
+    <Suspense fallback={<div className='flex flex-wrap gap-6 py-10 items-center justify-center'>
+        { Array.from({ length: 7 }, (_, index) => <Skeleton key={index} className='min-w-64 min-h-64 size-64 bg-[#e0e0e0] [animation-duration:1s]' />) }
+        </div>}
+    >
+        {children}
+    </Suspense>
+  )
+}
