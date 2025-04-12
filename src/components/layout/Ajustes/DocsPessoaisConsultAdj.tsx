221f85f8b819a8a021a85f152a24194cb54f7b1f
Lucas M. Lara <LucasML@Live.com>
2025-04-12 19:48:36 -0300

    WIP: iniciando o trabalho do componente dos docs pessoais por consultor (Componente em si)

--- src/components/layout/Ajustes/DocsPessoaisConsultAdj.tsx
+++ src/components/layout/Ajustes/DocsPessoaisConsultAdj.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { DocumentoConsultor } from "@/types/types";
 
-export default function DocsPessoaisConsultAdj() {
+export default function DocsPessoaisConsultAdj({
+  documentosDosConsultoresParaAjustes,
+}: {
+  documentosDosConsultoresParaAjustes: Record<string, DocumentoConsultor[]>;
+}) {
+  console.log(
+    "documentosDosConsultoresParaAjustes",
+    documentosDosConsultoresParaAjustes
+  );
   return <div>DocsPessoaisConsultAdj</div>;
 }
